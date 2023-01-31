8eefeccff24da2752b81302d6c3d32d744a05de5
hulyakucuk <0hulyakucuk@gmail.com>
2023-01-31 21:30:30 +0300

    update navigation bar

---
+++ responsive-navigation-bar/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+hamburger.onclick=function(){
+    const navbar=document.querySelector(".navbar");
+    navbar.classList.toggle("active");
+   
+}
