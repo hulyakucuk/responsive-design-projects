5b734468e1b9f94b85f1314a5afac317bb60f7c8
hulyakucuk <0hulyakucuk@gmail.com>
2023-02-01 23:07:24 +0300

    add media queries and update js code

--- responsive-website-01/script.js
+++ responsive-website-01/script.js
@@ -1,7 +1,9 @@
 const body =document.querySelector("body"),
       nav =document.querySelector("nav"),
       modeToggle=document.querySelector(".dark-light"),
-      searchToggle=document.querySelector(".searchToggle");
+      searchToggle=document.querySelector(".searchToggle"),
+      sidebarClose=document.querySelector(".sidebarClose"),
+      sidebarOpen=document.querySelector(".sidebarOpen");
 
 
 
@@ -12,6 +14,21 @@ const body =document.querySelector("body"),
 
       searchToggle.addEventListener("click",()=>{
         searchToggle.classList.toggle("active");
+ 
+      });
+
+      sidebarOpen.addEventListener("click",()=>{
+        nav.classList.add("active");
+ 
+      });
+
+      body.addEventListener("click", e =>{
+        let clickedElm=e.target;
+        if(!clickedElm.classList.contains("sidebarOpen") &&
+        !clickedElm.classList.contains("menu"))
+        {
+            nav.classList.remove("active");
+        } 
  
       });
 
